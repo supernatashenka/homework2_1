dd437249abd75a81f10370cf6f5c438a06a02504
Netology QA <obgynkostina@gmail.com>
2023-03-12 14:45:27 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13676;
+        int bonus = ticketPrice / 20;
+
+
+        System.out.println("Вам начислено " + bonus + " Миль");
+    }
+}
